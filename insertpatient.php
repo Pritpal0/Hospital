ccded56d512f7c87642f8abb7952d09c8dab1ed6
pritpalsingh94 <40607923+pritpalsingh94@users.noreply.github.com>
2019-12-01 22:05:23 -0600

    fixed add patient

--- insertpatient.php
+++ insertpatient.php
@@ -20,22 +20,7 @@ require 'dbcheck.inc.php';
 if (!empty($PatientID) || !empty($First) || !empty($Last) || !empty($SSN) || !empty($Gender) || !empty($Phone)|| !empty($Street)|| !empty($City)|| !empty($State)|| !empty($ZipCode)
 	|| !empty($Date_Of_Birth)|| !empty($Dept_ID)){
     
-    if (!preg_match("/^[0-9]*$/", $PatientID) 
-        || !preg_match("/^[a-zA-Z -]*$/", $First) 
-        || !preg_match("/^[a-zA-Z -]*$/", $Last) 
-        || !preg_match("/^[0-9]*$/", $SSN) ||  strlen($SSN) != 10
-        || !preg_match("/^[0-9]*$/", $Phone) ||  strlen($Phone) != 10
-        || !preg_match("/^[a-zA-Z@.0-9]*$/", $patient_email)
-        || !preg_match("/^[a-zA-Z0-9. ]*$/", $Street) 
-        || !preg_match("/^[a-zA-Z ]*$/", $City) 
-        || !preg_match("/^[a-zA-Z ]*$/", $State)
-        || !preg_match("/^[0-9]*$/", $ZipCode))
-    {
-        echo "ASDFASDF";
-        header("Location: /hospital /addpatient.php?appointment=invalid");
-        exit();
-    }
-    else{
+    
      $INSERT = "INSERT Into patient 
      (PatientID, First, Last, SSN, Gender, Phone, Street, City, State, ZipCode, Date_Of_Birth, Dept_ID ) 
      values('$PatientID', '$First', '$Last', '$SSN', '$Gender', '$Phone', '$Street', '$City', '$State', '$ZipCode', '$Date_Of_Birth', '$Dept_ID')";
@@ -51,7 +36,6 @@ if (!empty($PatientID) || !empty($First) || !empty($Last) || !empty($SSN) || !em
      } else{
          echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
      }
-    }
     
 } 
 else {
